f8717d330a0ad7e184a081f2be34c06b6f02b7e9
bramuel <jorammanoah1@gmail.com>
2021-09-22 06:33:16 -0700

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { uuid } from 'uuidv4'
+import AddItem from './AddItem';
+import Header from './Header'
+import ListItem from './ListItem';
 
 export default function App() {
+  const [items, setItems] = useState([
+    { id: uuid(), text: 'Milk' },
+    { id: uuid(), text: 'Eggs' },
+    { id: uuid(), text: 'Bread' },
+    { id: uuid(), text: 'Juice' },
+
+  ])
+
+  const deleteItem = (id) =>{
+    setItems(prevItems=>{
+      return prevItems.filter(item=>item.id != id)
+    })
+  }
+
+  const addItem = text =>{
+    setItems(prevItems=>{
+      return [{id:uuid(),text}, ...prevItems]
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Header />
+        <AddItem addItem={addItem} />
+        <FlatList
+          data={items}
+          renderItem={({ item }) => {
+            return <ListItem item={item} deleteItem={deleteItem}/>
+          }} />
+
+      </View>
+    </>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  text:{
+    color:"black"
+  }
 });
